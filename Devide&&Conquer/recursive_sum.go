1aa7f5af8e37ff7420930b20b58a2ac18fc03c74
bin7sk <bin7sk@gmail.com>
2022-08-30 21:23:47 +0300

    recursive sum, devide and conquer

---
+++ Devide&&Conquer/recursive_sum.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func sum(array []int) int {
+	if len(array) == 1 {
+		return array[0]
+	}
+	return array[0] + sum(array[1:])
+}
+
+func main() {
+	array := [...]int{4, 2, 3, 7, 9, 11}
+	var counted_sum int = sum(array[:])
+	fmt.Println(counted_sum)
+}
